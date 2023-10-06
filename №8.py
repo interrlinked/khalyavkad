0726b2c79d27c8d95491b6e58ccabf1bd1077223
Khalyavka Danyil <144445851+interrlinked@users.noreply.github.com>
2023-10-06 14:09:48 +0300

    Delete №8.py

--- №8.py
+++
@@ -1,8 +0,0 @@
-string = input("Введіть рядок слів, розділених пробілами: ")
-
-words = string.split()
-longest_word = max(words, key=len)
-word_length = len(longest_word)
-
-print("Найдовше слово:", longest_word)
-print("Довжина найдовшого слова:", word_length)
